44b3ff15eb81efa2e14b2a76df09061a8191352c
levankhelo <levankhelo@gmail.com>
2024-09-13 06:18:09 +0400

    feat: add docs

--- homework-6/run_integration_test.sh
+++ homework-6/run_integration_test.sh
@@ -15,30 +15,30 @@ fi
 echo "Starting Localstack..."
 docker-compose up -d
 
-# Wait until Localstack is ready
-echo "Waiting for Localstack to be ready..."
-until curl -s "$S3_ENDPOINT_URL/health" | grep "\"s3\": \"running\"" > /dev/null; do
-  sleep 5
-  echo "Waiting for Localstack S3 service to be ready..."
-done
-echo "Localstack is ready."
+sleep 15
 
 # Create the S3 bucket in Localstack if it doesn't exist
+echo "Creating bucket..."
 aws --endpoint-url="$S3_ENDPOINT_URL" s3 mb s3://nyc-duration || true
 
 # Create test data using the separate Python script
+echo "Creating test data..."
 python create_test_data.py
 
 # Upload the test data to S3
+echo "Uploading test data to S3..."
 aws --endpoint-url="$S3_ENDPOINT_URL" s3 cp test_input.parquet s3://nyc-duration/in/2023-01.parquet
 
 # Run the batch.py script for January 2023
+echo "Running batch.py script..."
 python batch.py 2023 1
 
 # Download the output from S3
+echo "Downloading output from S3..."
 aws --endpoint-url="$S3_ENDPOINT_URL" s3 cp s3://nyc-duration/out/2023-01.parquet output.parquet
 
 # Calculate the sum of predicted durations using the separate Python script
+echo "Calculating sum of predicted durations..."
 total_predicted_duration=$(python calculate_sum.py)
 
 echo "Total predicted duration: $total_predicted_duration"
